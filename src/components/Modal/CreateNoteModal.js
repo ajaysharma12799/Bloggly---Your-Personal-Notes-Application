0d52d8d565907f1f89863b40903bb0c756a0d743
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-02-02 17:57:15 +0530

    added User Specific Notes Approach

--- src/components/Modal/CreateNoteModal.js
+++ src/components/Modal/CreateNoteModal.js
@@ -5,7 +5,7 @@ import { AiOutlineClose } from 'react-icons/ai';
 import TextInput from '../common/TextInput';
 import TextArea from '../common/TextArea';
 import { useFormik } from 'formik';
-import { collection, Timestamp, doc, where, query, getDocs, updateDoc } from '@firebase/firestore';
+import { collection, Timestamp, addDoc } from '@firebase/firestore';
 import { auth, db } from '../../config/firebase';
 import { toast } from 'react-toastify';
 
@@ -15,40 +15,15 @@ const CreateNoteModal = ({ modalIsOpen, toggleModal }) => {
     const user = auth.currentUser;
     const handleAddFunctionality = async (notesObj) => {
         try {
-            console.log(notesObj);
-            const collectionRef = collection(db, 'users');
-            const qry = query(collectionRef, where('id', '==', user.uid));
-            const res = await getDocs(qry);
-
-            res.forEach((document) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(document.id, " => ", document.data());
-
-                const docRef = doc(db, 'users', document.id);
-                const notesData = document.data();
-
-                const { notes } = notesData;
-
-                const newNotesObj = {
-                    ...notesObj,
-                    createdAt: Timestamp.now(),
-                }
-
-                const notesArray = [...notes, newNotesObj];
-
-                updateDoc(docRef, {
-                    notes: notesArray
-                })
-                    .then((e) => {
-                        toggleModal();
-                        toast.success('Note Created Sucessfully');
-                        formik.resetForm();
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    });
-
+            await addDoc(collection(db, 'notes'), {
+                ...notesObj,
+                userID: user.uid,
+                createdAt: Timestamp.now(),
             });
+
+            toggleModal();
+            toast.success('Note Created Sucessfully');
+            formik.resetForm();
         } catch (error) {
             console.log(error);
             toast.error(error);
